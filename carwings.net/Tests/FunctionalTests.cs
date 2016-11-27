16b30f83c5592c71ca06b75c87ac0825fbd83af0
StephenE <sedevo-github@yahoo.co.uk>
2016-11-27 12:17:33 +0000

    Fixed up to work with current carwings API

--- carwings.net/Tests/FunctionalTests.cs
+++ carwings.net/Tests/FunctionalTests.cs
@@ -1,5 +1,8 @@
 ﻿using carwings.net;
+using carwings.net.login.bouncycastle;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net;
+using System.Threading;
 
 namespace Tests
 {
@@ -11,7 +14,17 @@ namespace Tests
         {
             var carwings = GetCarwings();
             var userLoginResponse = carwings.Login().Result;
-            var vehicle = userLoginResponse.Profile;
+
+            Assert.IsNotNull(userLoginResponse);
+            Assert.AreEqual(200, userLoginResponse.Status);
+
+            Assert.IsNotNull(userLoginResponse.Vehicles);
+            Assert.AreEqual(1, userLoginResponse.Vehicles.Count);
+            Assert.IsNotNull(userLoginResponse.Vehicles[0]);
+            Assert.IsNotNull(userLoginResponse.Vehicles[0].CustomSessionId);
+
+            Assert.IsNotNull(userLoginResponse.Profile);
+            Assert.IsNotNull(userLoginResponse.Profile.Vin);
         }
 
         [TestMethod]
@@ -20,7 +33,9 @@ namespace Tests
             var carwings = GetCarwings();
             var userLoginResponse = carwings.Login().Result;
             var vehicle = userLoginResponse.Profile;
-            var batteryStatus = carwings.GetBatteryStatus(vehicle).Result;
+            var batteryStatus = carwings.GetBatteryStatus(userLoginResponse.Vehicles[0], vehicle).Result;
+
+            Assert.IsNotNull(batteryStatus);
         }
 
         [TestMethod]
@@ -29,12 +44,14 @@ namespace Tests
             var carwings = GetCarwings();
             var userLoginResponse = carwings.Login().Result;
             var vehicle = userLoginResponse.Profile;
-            var checkRequest = carwings.RefreshBatteryStatus(vehicle).Result;
+            var checkRequest = carwings.RefreshBatteryStatus(userLoginResponse.Vehicles[0], vehicle).Result;
 
             BatteryStatusCheckResultResponse batteryStatusCheckResult;
             do
             {
-                batteryStatusCheckResult = carwings.CheckBatteryStatus(vehicle, checkRequest).Result;
+                Thread.Sleep(5000);
+                batteryStatusCheckResult = carwings.CheckBatteryStatus(userLoginResponse.Vehicles[0], vehicle, checkRequest).Result;
+                Assert.IsNotNull(batteryStatusCheckResult);
             }
             while (batteryStatusCheckResult.ResponseFlag != 1);
         }
@@ -45,11 +62,56 @@ namespace Tests
             var carwings = GetCarwings();
             var userLoginResponse = carwings.Login().Result;
             var vehicle = userLoginResponse.Profile;
-            var hvacStatus = carwings.GetHvacStatus(vehicle).Result;
+            var hvacStatus = carwings.GetHvacStatus(userLoginResponse.Vehicles[0], vehicle).Result;
+
+            Assert.IsNotNull(hvacStatus);
+        }
+
+        [TestMethod]
+        public void TurnHvacOn()
+        {
+            var carwings = GetCarwings();
+            var userLoginResponse = carwings.Login().Result;
+            var vehicle = userLoginResponse.Profile;
+            var checkRequest = carwings.HvacOn(userLoginResponse.Vehicles[0], vehicle).Result;
+
+            HvacStatusCheckResultResponse hvacStatusCheckResultResponse;
+            do
+            {
+                Thread.Sleep(5000);
+                hvacStatusCheckResultResponse = carwings.CheckHvacOnStatus(userLoginResponse.Vehicles[0], vehicle, checkRequest).Result;
+                Assert.IsNotNull(hvacStatusCheckResultResponse);
+            }
+            while (hvacStatusCheckResultResponse.ResponseFlag != 1);
+        }
+
+        [TestMethod]
+        public void TurnHvacOff()
+        {
+            var carwings = GetCarwings();
+            var userLoginResponse = carwings.Login().Result;
+            var vehicle = userLoginResponse.Profile;
+            var checkRequest = carwings.HvacOff(userLoginResponse.Vehicles[0], vehicle).Result;
+
+            HvacStatusCheckResultResponse hvacStatusCheckResultResponse;
+            do
+            {
+                Thread.Sleep(5000);
+                hvacStatusCheckResultResponse = carwings.CheckHvacOffStatus(userLoginResponse.Vehicles[0], vehicle, checkRequest).Result;
+                Assert.IsNotNull(hvacStatusCheckResultResponse);
+            }
+            while (hvacStatusCheckResultResponse.ResponseFlag != 1);
         }
 
         private Carwings GetCarwings()
         {
-            return new Carwings(Region.USA, "username", "password");       }
+            // Disable HTTPS verification, to allow using Fiddler
+            ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) => true;
+
+            // Use BouncyCastle implementation of password provider
+            LoginProvider loginProvider = new LoginProvider("Username", "Password");
+
+            return new Carwings(Region.Europe, loginProvider);
+        }
     }
 }
