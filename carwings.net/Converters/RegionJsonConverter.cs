662a63a05c5965a4ac0a124b1b9f54e287f713f3
sirfergy <sean@thefergies.net>
2018-09-08 09:55:13 -0700

    Add files

---
+++ carwings.net/Converters/RegionJsonConverter.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace carwings.net
+{
+    public class RegionJsonConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(string);
+        }
+
+        public override bool CanWrite
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            var regionCode = (string)reader.Value;
+            switch (regionCode)
+            {
+                case "NMA":
+                    return Region.Australia ;
+                case "NCI":
+                    return Region.Canada;
+                case "NE":
+                    return Region.Europe;
+                case "NML":
+                    return Region.Japan;
+                case "NNA":
+                    return Region.USA;
+            }
+
+            throw new ArgumentOutOfRangeException(nameof(regionCode));
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            var region = (Region)value;
+            switch (region)
+            {
+                case Region.Australia:
+                    writer.WriteValue("NMA");
+                    break;
+                case Region.Canada:
+                    writer.WriteValue("NCI");
+                    break;
+                case Region.Europe:
+                    writer.WriteValue("NE");
+                    break;
+                case Region.Japan:
+                    writer.WriteValue("NML");
+                    break;
+                case Region.USA:
+                    writer.WriteValue("NNA");
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(region), "Unsupported region");
+            }
+        }
+    }
+}
---
+++ carwings.net/Region.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace carwings.net
+{
+    public enum Region
+    {
+        Australia,
+        Canada,
+        Europe,
+        Japan,
+        USA,
+    }
+
+    public static class RegionExtensions
+    {
+        public static string ToRegionCode(this Region region)
+        {
+            switch (region)
+            {
+                case Region.Australia:
+                    return "NMA";
+                case Region.Canada:
+                    return "NCI";
+                case Region.Europe:
+                    return "NE";
+                case Region.Japan:
+                    return "NML";
+                case Region.USA:
+                    return "NNA";
+            }
+
+            throw new ArgumentOutOfRangeException(nameof(region), "Unsupported region");
+        }
+    }
+}
