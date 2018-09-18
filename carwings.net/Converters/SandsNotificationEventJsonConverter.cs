e770e695ef1f46185ff7bfae48607782879545f1
Brian Grunkemeyer <brian@flexcharging.com>
2018-09-17 20:35:46 -0700

    Looks like the location can be null too sometimes.

--- carwings.net/Converters/SandsNotificationEventJsonConverter.cs
+++ carwings.net/Converters/SandsNotificationEventJsonConverter.cs
@@ -13,6 +13,9 @@ namespace carwings.net
 
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
         {
+            if (reader.TokenType == JsonToken.Null)
+                return null;
+
             var obj = JObject.Load(reader);
 
             var sandsNotificationEvent = obj.SelectToken("sandsNotificationEvent.body.location");
